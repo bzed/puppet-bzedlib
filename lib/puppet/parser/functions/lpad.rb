9d5227595e0b3f3f8eb87cd39538350471052ac8
Bernd Zeimetz <bernd@bzed.de>
2016-01-21 18:46:03 +0100

    Add a simple lpad function

---
+++ lib/puppet/parser/functions/lpad.rb
@@ -0,0 +1,25 @@
+module Puppet::Parser::Functions
+ newfunction(:lpad, :type => :rvalue, :doc => <<-EOS
+
+Copyright (C) 2015 Bernd Zeimetz <b.zeimetz@conova.com>
+
+Simple ruby lpad function.
+lpad(integer, count)
+
+lpad(123, 6) returns "000123"
+
+EOS
+) do |args|
+
+    raise(Puppet::ParseError, "lpad(): Wrong number of arguments " +
+          "given (#{arguments.size} instead of 2)") if args.size != 2
+
+    num, count = args
+    num = num.to_i
+    count = count.to_i
+
+    format = "%0" + count + "d"
+    return format % num
+  end
+end
+
